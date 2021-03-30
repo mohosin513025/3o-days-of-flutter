4d629551f9f23d4a67f4837873d6e3c078aedf63
mohosin513025 <36011293+mohosin513025@users.noreply.github.com>
2021-03-30 18:03:02 +0530

    day2

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "mohosin akhan";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("catalog app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("$days days flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_03/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,13 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Material(
-          child: Center(
-            child: Container(
-              child: Text("30 days flutter"),
-            ),
-          ),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
