119e10678b956dec3c98232904d61d7574cbd254
mohosin513025 <36011293+mohosin513025@users.noreply.github.com>
2021-03-30 17:13:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Material(
+          child: Center(
+            child: Container(
+              child: Text("30 days flutter"),
+            ),
+          ),
+        ));
+  }
+}
