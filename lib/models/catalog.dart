cdebca4a760df801f0102ce4b342aae4479a6e67
mohosin513025 <36011293+mohosin513025@users.noreply.github.com>
2021-04-07 13:29:55 +0530

    DAY9 COMPLETED

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+final products = [
+  Item(
+    id: "test001",
+    name: "galaxy s32",
+    desc: "Samsung galaxy S 32 Pro",
+    price: 16999;
+    color: "#090909",
+    image: "https://www.tatacliq.com/samsung-galaxy-a32-128-gb-awesome-blue-6-gb-ram-dual-sim-4g/p-mp000000008976548?cid=ps:E_sok-tatacliq-shopping-ssc-mobiles-accessories:Google:PLA_Electronincs&gclid=Cj0KCQjwsLWDBhCmARIsAPSL3_2e7NWhgz8xXjd4f3B_K0s5FN1M7cTGnBB8CLtOdHIzTYZo2dwj9lIaAnF9EALw_wcB",
+
+  )
+];
